62a7a033c3b7ec57f9d0e8c15ef6d253b717701d
Takeout55 <carwyn-james@outlook.com>
2026-05-29 00:57:43 +0100

    Added saving UUID data as txt file

--- UUID Lookup.py
+++ UUID Lookup.py
@@ -1,14 +1,26 @@
 import requests
+import os.path
 
 question = input("Username or UUID?")
 if question == "Username":
     Username = input("Username?")
     url = "https://api.mojang.com/users/profiles/minecraft/" + Username
+    filename=Username
 
 if question == "UUID":
     UUID = input("UUID?")
     url = "https://api.minecraftservices.com/minecraft/profile/lookup/" + UUID
+    filename=UUID
 
 resp = requests.get(url=url)
 data = resp.json()
 print(data)
+
+if os.path.isfile("files/" + filename + ".txt"):
+    f = open("files/" + filename + ".txt", "x")
+    f.write(str(data))
+    f.close()
+else:
+    f = open("files/" + filename + ".txt", "x")
+    f.write(str(data))
+    f.close()
