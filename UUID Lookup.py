656189d8990e87c96adf194c3aad189569decfbb
Takeout55 <carwyn-james@outlook.com>
2026-07-16 21:40:44 +0100

    Added Restart Option

--- UUID Lookup.py
+++ UUID Lookup.py
@@ -1,32 +1,40 @@
 import requests
 import os.path
 
-question = input("Username or UUID?")
-if question == "Username":
-    Username = input("Username?")
-    url = "https://api.mojang.com/users/profiles/minecraft/" + Username
-    filename=Username
+while True:
+    question = input("Username or UUID?")
+    if question == "Username":
+        Username = input("Username?")
+        url = "https://api.mojang.com/users/profiles/minecraft/" + Username
+        filename=Username
 
-if question == "UUID":
-    UUID = input("UUID?")
-    url = "https://api.minecraftservices.com/minecraft/profile/lookup/" + UUID
-    filename=UUID
+    if question == "UUID":
+        UUID = input("UUID?")
+        url = "https://api.minecraftservices.com/minecraft/profile/lookup/" + UUID
+        filename=UUID
 
-resp = requests.get(url=url)
-data = resp.json()
-print(data)
+    resp = requests.get(url=url)
+    data = resp.json()
+    print(data)
 
-if os.path.isdir("files"):
-    if os.path.isfile("files/" + filename + ".txt"):
-        f = open("files/" + filename + ".txt", "w")
-        f.write(str(data))
-        f.close()
+    if os.path.isdir("files"):
+        if os.path.isfile("files/" + filename + ".txt"):
+            f = open("files/" + filename + ".txt", "w")
+            f.write(str(data))
+            f.close()
+        else:
+            f = open("files/" + filename + ".txt", "x")
+            f.write(str(data))
+            f.close()
     else:
+        os.mkdir("files")
         f = open("files/" + filename + ".txt", "x")
         f.write(str(data))
         f.close()
-else:
-    os.mkdir("files")
-    f = open("files/" + filename + ".txt", "x")
-    f.write(str(data))
-    f.close()
+
+    question1 = input("Restart? Y/N")
+    question1.upper()
+    if question1 == "Y":
+        continue
+    elif question1 == "N":
+        break
